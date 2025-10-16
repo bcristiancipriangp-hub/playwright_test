ea321b82e9b650b9e30789b89ad4f55472a454be
bcristiancipriangp-hub <bcristiancipriangp@gmail.com>
2025-10-16 12:35:10 +0300

    updated e2e for allure 5th

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['allure-playwright', { outputFolder: 'test-results' }],
+    ['html'],
+    ['list'],
+    ['allure-playwright', { outputFolder: 'test-results' }], 
+    ['github']
     ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: require.resolve('./utils/global-setup'),
